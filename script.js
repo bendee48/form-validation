3be09c2a58e1317bad878275a85ea1af30f694c2
Ben Daniels <benbanj@hotmail.com>
2023-01-26 21:29:46 +0000

    Adding Functionality to Verify Country
    - Adding function to validate country
    - Adding validation for presence, length and all letters
    - Moving presence check into own helper function

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 const email = document.querySelector('input[name="email"]');
 const emailErrors = document.querySelector('.errors-email');
-
-
+const country = document.querySelector('input[name="country"]');
+const countryErrors = document.querySelector('.errors-country');
 email.addEventListener('focusout', validateEmail);
+country.addEventListener('focusout', validateCountry);
 
 function validateEmail() {
   const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
   let errorText = '';
 
-  if (!email.value) {
-    errorText += 'This field is required';
-    email.className = 'invalid';
-    emailErrors.textContent = errorText;
+  if (!inputIsPresent(email, errorText, emailErrors)) {
     return false;
-  };
+  }
+
   if (!emailRegex.test(email.value)) {
     errorText += 'Not a valid email address';
     email.className = 'invalid';
@@ -23,5 +22,41 @@ function validateEmail() {
 
   email.className = 'valid';
   emailErrors.textContent = '';
+  return true;
+}
+
+function validateCountry() {
+  let errorText = '';
+
+  if (!inputIsPresent(country, errorText, countryErrors)) {
+    return false;
+  }
+
+  if (country.value.length > 30) {
+    errorText += 'Must be shorter than 30 chars';
+    country.className = 'invalid';
+    countryErrors.textContent = errorText;
+    return false;
+  }
+
+  if (/\d/.test(country.value)) {
+    errorText += 'Country shouldn\'t contain any numbers';
+    country.className = 'invalid';
+    countryErrors.textContent = errorText;
+    return false;
+  }
+
+  country.className = 'valid';
+  countryErrors.textContent = '';
+  return true;
+}
+
+function inputIsPresent(inputElement, errorText, errorContainer) {
+  if (!inputElement.value) {
+    errorText += 'This field is required';
+    inputElement.className = 'invalid';
+    errorContainer.textContent = errorText;
+    return false;
+  }
   return true;
 }
